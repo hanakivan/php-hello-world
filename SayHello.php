31909fb847de4e7af938659413d88674dd5dd201
Ivan Hanak <hanakivan@gmail.com>
2020-07-16 22:33:58 +0200

    Added a new feature

--- SayHello.php
+++ SayHello.php
@@ -2,11 +2,13 @@
 
 namespace HelloWorld;
 
+use Carbon\Carbon;
+
 class SayHello
 {
     public static function world()
     {
-        return 'Hello World, Composer!';
+        return sprintf('Hello World, Composer! Today is %s.', Carbon::now()->toDateTimeLocalString());
     }
 
 }
